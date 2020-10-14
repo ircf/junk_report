9b3d0104c0bb502ab3db3a3db1f6afabc35c8882
Mathieu Bautista <mbautista@ircf.fr>
2020-10-14 09:51:45 +0200

    added settings form (wip)

--- junk_report.php
+++ junk_report.php
@@ -1,12 +1,23 @@
 <?php
+/**
+ * Junk Report
+ *
+ * Send periodically an email to each mailbox with the list of messages in their junk directory
+ *
+ * @version 1.0
+ * @author IRCF
+ * @url https://github.com/ircf/junk_report
+ */
 class junk_report extends rcube_plugin
 {
+  const DEFAULT_FREQUENCY = 'never';
+  const DEFAULT_MAXLENGTH = 50;
   public $task = 'settings';
-  private $rcmail_inst;
+  private $rcmail;
 
   function init()
   {
-    $this->rcmail_inst = rcmail::get_instance();
+    $this->rcmail = rcmail::get_instance();
     $this->load_config();
     $this->add_texts('localization/', true);
     $this->require_plugin('markasjunk2');
@@ -17,15 +28,70 @@ class junk_report extends rcube_plugin
     $this->register_action('plugin.junk_report.not_junk', array($this, 'not_junk'));
     $this->register_action('plugin.junk_report.cron', array($this, 'cron'));
 
-    //$this->api->output->add_handler('junk_report_form', array($this, 'init_form'));
+    $this->add_hook('settings_actions', array($this, 'settings_actions'));
+  }
+
+  function settings_actions($args)
+  {
+    $args['actions'][] = array(
+      'action' => 'plugin.junk_report.show',
+      'class'  => 'junk_report',
+      'label'  => 'title',
+      'domain' => 'junk_report',
+    );
+    return $args;
+  }
+
+  function init_html()
+  {
+    $this->register_handler('plugin.body', array($this, 'show'));
+    $this->rcmail->output->set_pagetitle($this->gettext('title'));
+    $this->rcmail->output->send('plugin');
   }
 
   /**
    * display settings form
    */
-  function init_html()
+  function show()
   {
-    // TODO
+    // TODO read from user preferences
+    $frequency = self::DEFAULT_FREQUENCY;
+    $maxlength = self::DEFAULT_MAXLENGTH;
+
+    $table = new html_table(array('cols' => 2, 'class' => 'propform'));
+
+    $table->add('title', html::label('', 'frequency'));
+    $table->add('',  html::tag('input', array(
+      'type' => 'text',
+      'name' => '_frequency',
+      'value' => $frequency
+    )));
+
+    $table->add('title', html::label('', 'maxlength'));
+    $table->add('',  html::tag('input', array(
+      'type' => 'text',
+      'name' => '_maxlength',
+      'value' => $maxlength
+    )));
+
+    return html::tag('form', array(
+        'action' => $this->rcmail->url('plugin.junk_report.save'),
+        'method' => 'post'
+      ),
+      html::div(array('class' => 'box formcontent'),
+        html::div(array('class' => 'boxtitle'), $this->gettext('title'))
+        . html::div(array('class' => 'boxcontent'),
+          html::p('', $this->gettext('description'))
+          . '<br>'
+          . $table->show()
+          . html::p(array('class' => 'formbuttons'),
+            html::tag('input', array('type' => 'submit',
+              'class' => 'button mainaction', 'value' => $this->gettext('save'))
+            )
+          )
+        )
+      )
+    );
   }
 
   /**
